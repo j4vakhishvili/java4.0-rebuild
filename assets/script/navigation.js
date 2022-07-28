9151bafc43e7b3fe74dd8facba9c6eb9d33f76ab
J4vakhishvili <71881956+j4vakhishvili@users.noreply.github.com>
2022-07-28 13:36:35 +0400

    return 0

--- assets/script/navigation.js
+++ assets/script/navigation.js
@@ -1,12 +1,11 @@
 const navbar = document.querySelector('nav');
-const logo = document.getElementById('logo');
 
 function openNav(){
     navbar.style.transform = "scale(1)";
-    logo.style.opacity = "0";
+    navbar.style.top = "0";
 }
 
 function closeNav(){
     navbar.style.transform = "scale(0)";
-    logo.style.opacity = "1";
+    navbar.style.top = "-100%";
 }
