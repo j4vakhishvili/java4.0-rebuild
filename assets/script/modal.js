e69f1503ba6d259a85819b360f7b9e42f210a477
J4vakhishvili <71881956+j4vakhishvili@users.noreply.github.com>
2022-10-01 22:18:54 +0400

    modal

---
+++ assets/script/modal.js
@@ -0,0 +1,13 @@
+let modal = document.querySelector('.modal');
+let width = modal.offsetWidth;
+
+let close = document.querySelector('.close');
+close.style.width = width + "px";
+
+let modalTwo = document.querySelector('.modalTwo');
+let widthTwo = modalTwo.offsetWidth;
+
+let closeTwo = document.querySelector('.closeTwo');
+closeTwo.style.width = widthTwo + "px";
+
+console.log({width});
--- assets/script/navigation.js
+++ assets/script/navigation.js
@@ -1,11 +1,33 @@
 const navbar = document.getElementById('menu');
+const about = document.getElementById('aboutMe');
+const resume = document.getElementById('resume');
 
 function openNav(){
-    navbar.style.transform = "translateX(0)";
+    navbar.style.transform = "translateY(0)";
     navbar.style.opacity = "1";
 }
 
 function closeNav(){
-    navbar.style.transform = "translateX(-100%)";
+    navbar.style.transform = "translateY(-100%)";
     navbar.style.opacity = "0.6";
+}
+
+function openAbout(){
+    about.style.transform = "translateY(0)";
+    about.style.opacity = "1";
+}
+
+function closeAbout(){
+    about.style.transform = "translateY(-100%)";
+    about.style.opacity = "0.6";
+}
+
+function openResume(){
+    resume.style.transform = "translateY(0)";
+    resume.style.opacity = "1";
+}
+
+function closeResume(){
+    resume.style.transform = "translateY(-100%)";
+    resume.style.opacity = "0.6";
 }
