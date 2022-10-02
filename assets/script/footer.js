bac2baf21d661fa66237f573f03e4a15b0522314
J4vakhishvili <71881956+j4vakhishvili@users.noreply.github.com>
2022-10-02 13:55:57 +0400

    footer

--- assets/script/footer.js
+++ assets/script/footer.js
@@ -1,3 +1,14 @@
+//You can CHANGE thoose
+let nameOne = "YouTube";
+let nameTwo = "Dribbble";
+let nameThree = "Facebook";
+
+var openUrlOne = 'https://www.youtube.com/j4vakhishvili';
+var openUrlTwo = 'https://dribbble.com/J4vakhishvili';
+var openUrlThree = 'https://www.facebook.com/J4vakhishvili/';
+
+
+//Do not tuch them!!!
 const socialNameOne = document.getElementById("socialNameOne");
 const socialNameTwo = document.getElementById("socialNameTwo");
 const socialNameThree = document.getElementById("socialNameThree");
@@ -6,13 +17,19 @@ const socialLinkOne = document.getElementById("socialLinkOne");
 const socialLinkTwo = document.getElementById("socialLinkTwo");
 const socialLinkThree = document.getElementById("socialLinkThree");
 
-let nameOne = "Gamrjoba";
-let nameTwo = "Gamrjoba";
-let nameThree = "Gamrjoba";
-
 socialNameOne.innerHTML = nameOne; 
 socialLinkOne.innerHTML = nameOne;
 socialNameTwo.innerHTML = nameTwo; 
 socialLinkTwo.innerHTML = nameTwo;
 socialNameThree.innerHTML = nameThree; 
 socialLinkThree.innerHTML = nameThree;
+
+function urlOne(){
+    window.open(openUrlOne, '_blank');
+}
+function urlTwo(){
+    window.open(openUrlTwo, '_blank');
+}
+function urlThree(){
+    window.open(openUrlThree, '_blank');
+}
