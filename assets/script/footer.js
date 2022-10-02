50a3ef7c62cb97db133504083a3f3aea993c250d
J4vakhishvili <71881956+j4vakhishvili@users.noreply.github.com>
2022-10-02 10:39:37 +0400

    footer

---
+++ assets/script/footer.js
@@ -0,0 +1,18 @@
+const socialNameOne = document.getElementById("socialNameOne");
+const socialNameTwo = document.getElementById("socialNameTwo");
+const socialNameThree = document.getElementById("socialNameThree");
+
+const socialLinkOne = document.getElementById("socialLinkOne");
+const socialLinkTwo = document.getElementById("socialLinkTwo");
+const socialLinkThree = document.getElementById("socialLinkThree");
+
+let nameOne = "Gamrjoba";
+let nameTwo = "Gamrjoba";
+let nameThree = "Gamrjoba";
+
+socialNameOne.innerHTML = nameOne; 
+socialLinkOne.innerHTML = nameOne;
+socialNameTwo.innerHTML = nameTwo; 
+socialLinkTwo.innerHTML = nameTwo;
+socialNameThree.innerHTML = nameThree; 
+socialLinkThree.innerHTML = nameThree;
