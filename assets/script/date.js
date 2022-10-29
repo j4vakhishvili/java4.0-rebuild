77a96ccc5296b0b30a71afd4799e86b0e49b30c5
J4vakhishvili <71881956+j4vakhishvili@users.noreply.github.com>
2022-10-29 16:15:31 +0400

    fix?

--- assets/script/date.js
+++ assets/script/date.js
@@ -8,6 +8,17 @@ let calendar = date.getMonth();
 console.log({calendar});
 
 
+let snowflakes = document.getElementById("winterCSS");
+let snowflakesHTML = document.getElementById("snowflakes");
+
+snowflakes.disabled = true;
+snowflakesHTML.style.display = "none";
+
+if(calendar == "11" || calendar == "0"){
+    header.style.background = "url(https://cdn4.vectorstock.com/i/1000x1000/62/58/winter-night-vector-4226258.jpg)";
+    snowflakes.disabled = false;
+    snowflakesHTML.style.display = "block";
+}
 //header-background
 let header = document.querySelector('header');
 var imgCount = 4;
@@ -20,15 +31,3 @@ var imgCount = 4;
             images[4] = "wineweb.jpg",
             // header.style.background = "url(" + dir + images[randomCount] + "), #fb2477";
             header.style.background = "url("https://russiannobility.org/wp-content/uploads/2022/03/Flag_of_Ukraine.svg.png"), #fb2477";
-
-let snowflakes = document.getElementById("winterCSS");
-let snowflakesHTML = document.getElementById("snowflakes");
-
-snowflakes.disabled = true;
-snowflakesHTML.style.display = "none";
-
-if(calendar == "11" || calendar == "0"){
-    header.style.background = "url(https://cdn4.vectorstock.com/i/1000x1000/62/58/winter-night-vector-4226258.jpg)";
-    snowflakes.disabled = false;
-    snowflakesHTML.style.display = "block";
-}
